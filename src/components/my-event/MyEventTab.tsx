68e4800ca94da15f998e2aa3c791fcb1c64b34b6
sogobanwo <sogobanwo@gmail,com>
2025-04-05 09:43:24 +0100

    fix: general UI fixes

--- src/components/my-event/MyEventTab.tsx
+++ src/components/my-event/MyEventTab.tsx
@@ -1,13 +1,11 @@
 "use client";
 import React from "react";
 import MyEventTicket from "./Ticket";
-// import EventSpok from "./EventSpok";
 
 const MyEventTab = ({ ticket_price }: any) => {
   return (
     <div className={"w-full mb-8"}>
       <MyEventTicket ticket_price={ticket_price} />
-      {/* <EventSpok /> */}
     </div>
   );
 };
