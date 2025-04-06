3d5d90365cafda1e665ee76a08c49a2518b1efe1
sogobanwo <sogobanwo@gmail,com>
2025-04-06 15:18:42 +0100

    fix: ui fixes

--- src/components/dashboard/analytics-tabs/AnalyticsReview.tsx
+++ src/components/dashboard/analytics-tabs/AnalyticsReview.tsx
@@ -4,15 +4,18 @@ type Props = {}
 
 const AnalyticsReview = (props: Props) => {
   return (
-    <div className='border-b border-white/80 mb-6 flex gap-2 pb-6 flex-col'>
-        <div className='flex items-center gap-4'>
-            <img src="https://res.cloudinary.com/dnohqlmjc/image/upload/v1742633485/event-attendee-two_vhycvk.png" width={50} height={50} alt="attendee-image" />
-            <div>
-                <h6 className='text-white text-xl'>0x1234****</h6>
-                <p className='text-white/80 text-sm'>sogobanwo@gmail.com</p>
-            </div>
-        </div>
-        <p className='text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestias harum delectus ex sint sequi quaerat perspiciatis maxime ab nostrum.</p>
+    <div className='border-b border-white/80 mb-6 flex gap-2 pb-6 flex-col items-center justify-center text-center py-8'>
+      <img 
+        src="https://res.cloudinary.com/dnohqlmjc/image/upload/v1742633485/empty-reviews_vhycvk.png" 
+        width={80} 
+        height={80} 
+        alt="empty-reviews-icon" 
+        className="opacity-60"
+      />
+      <h6 className='text-white text-xl mt-4'>No Reviews Yet</h6>
+      <p className='text-white/80 text-sm max-w-md'>
+        Once attendees start leaving reviews for your event, they will appear here.
+      </p>
     </div>
   )
 }
