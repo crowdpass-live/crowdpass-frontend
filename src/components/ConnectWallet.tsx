fd74f0ca6cb0440670e658e599b88d1ee7a06e17
manoahLinks <manoahluka@gmail.com>
2025-05-03 11:31:17 +0100

    feat: integrate cartridge controller connect

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -1,18 +1,45 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { StarknetContext } from "@/contexts/UserContext";
+import { useAccount, useConnect, useDisconnect } from '@starknet-react/core'
+import {ControllerConnector} from '@cartridge/connector'
+
 
 const ConnectWalletButton = ({showMobileNav}: any) => {
   const { handleConnect }: any = useContext(StarknetContext);
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect()
+  const { address } = useAccount()
+  const controller = connectors[0] as ControllerConnector
+  const [username, setUsername] = useState<string>()
 
+  useEffect(() => {
+    if (!address) return
+    controller.username()?.then((n) => setUsername(n))
+  }, [address, controller])
+ 
  
   return (
+    <>
     <Button
       onClick={handleConnect}
       className={`bg-transaparent text-white font-semibold border border-white text-sm lg:text-base raleway hover:bg-primary hover:text-black hover:border-primary lg:ml-4 lg:py-6 lg:px-6 md:flex ${showMobileNav ? "block mb-3": ""}`}
     >
       Log in
     </Button>
+
+   { !address ? <Button
+      onClick={() => connect({ connector: controller })}
+      className={`bg-transaparent text-white font-semibold border border-white text-sm lg:text-base raleway hover:bg-primary hover:text-black hover:border-primary lg:ml-4 lg:py-6 lg:px-6 md:flex ${showMobileNav ? "block mb-3": ""}`}
+    >
+      Cartridge
+    </Button> : <>
+      <p>{username}</p> 
+      <button className="border border-orange-400 px-2 " onClick={() => disconnect()}>
+          disconnect
+        </button>
+    </>}
+    </>
   );
 };
 
--- src/components/StarknetProvider.tsx
+++ src/components/StarknetProvider.tsx
@@ -1,21 +1,65 @@
 "use client";
 import React from "react";
-import { sepolia } from "@starknet-react/chains";
+import { Chain, sepolia, mainnet } from "@starknet-react/chains";
 import { WebWalletConnector } from "starknetkit/webwallet";
-import {
-  StarknetConfig,
-  publicProvider,
-  voyager
-} from "@starknet-react/core";
+import { StarknetConfig, publicProvider, voyager, jsonRpcProvider} from "@starknet-react/core";
+import { SessionPolicies } from "@cartridge/controller";
+import { ControllerConnector } from "@cartridge/connector";
+import { constants } from "starknet";
+
+const policies: SessionPolicies = {
+  contracts: {
+    "0x076278c1c8e8cfd6c8304681bcf3670c2dd6751bc12b4e1d7d717dc01f7aa130": {
+      name: "wordle",
+      description: "contains the logic for a wordle game on starknet",
+      methods: [
+        {
+          name: "Start",
+          description: "starts a classic game",
+          entrypoint: "start"
+        }
+      ]
+    },
+   
+  },
+}
+
+// Configure RPC provider
+const provider = jsonRpcProvider({
+  rpc: (chain: Chain) => {
+    switch (chain) {
+      case mainnet:
+        return { nodeUrl: 'https://api.cartridge.gg/x/starknet/mainnet' }
+      case sepolia:
+      default:
+        return { nodeUrl: 'https://api.cartridge.gg/x/starknet/sepolia' }
+    }
+  },
+})
+
+  // Initialize the connector
+  const cartridgeConnector = new ControllerConnector({
+    policies,
+    chains: [
+      {
+        rpcUrl: "https://api.cartridge.gg/x/starknet/sepolia",
+      },
+    ],
+    defaultChainId: constants.StarknetChainId.SN_SEPOLIA,
+  });
  
+
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
-    const connector = new WebWalletConnector({ url: "https://web-v2.hydrogen.argent47.net/email" })
-    
+  const connector = new WebWalletConnector({
+    url: "https://web-v2.hydrogen.argent47.net/email",
+  });
+
   return (
     <StarknetConfig
+      autoConnect
       chains={[sepolia]}
-      provider={publicProvider()}
-      connectors={[connector]}
+      provider={provider}
+      connectors={[cartridgeConnector]}
       explorer={voyager}
     >
       {children}
