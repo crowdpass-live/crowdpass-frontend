a625ecea0d006d5ef1057159b2ba5652c9c1b354
Olorunsogo Banwo <bolorunsogo@efficion.org>
2025-08-24 05:57:37 +0100

    fix: deployment not showing

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Permissions-Policy',
-            value: 'payment=*, geolocation=*, microphone=*, camera=*, fullscreen=*'
-          },
-        ],
-      },
-    ];
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.optimization = {
-      ...config.optimization,
-      concatenateModules: false,
-    };
-
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      },
-    };
-
-    return config;
-  },
-  swcMinify: true,
-  experimental: {
-    esmExternals: 'loose',
-    forceSwcTransforms: true,
-  },
-};
-
+    images: {
+      domains: ['ipfs.io', 'res.cloudinary.com'],
+    },
+  };
 export default nextConfig;
